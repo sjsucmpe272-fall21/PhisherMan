da4eaa0233a6dc7824e42d503003011fcb469995
psalire <philip.salire@sjsu.edu>
2021-10-17 23:01:34 -0700

    add tabs and storage apis

--- chrome-extension/src/ts/background.ts
+++ chrome-extension/src/ts/background.ts
@@ -1,4 +1,20 @@
 
+const KEY_LAST_URL = "LAST_URL";
+
 chrome.runtime.onInstalled.addListener(() => {
     console.log("Installed");
+});
+
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+    console.log("tabs.onUpdated");
+    let activeUrl = tab.url;
+    console.log(activeUrl);
+    console.log(tab);
+
+    chrome.storage.local.get(KEY_LAST_URL, (items) => {
+        if (items[KEY_LAST_URL] == activeUrl) {
+            return; // Prevent page's URL being sent several times
+        }
+        chrome.storage.local.set({ [KEY_LAST_URL]: activeUrl });
+    });
 });
